22f8b9d23d441f5eb4dc9a2a0637d8e4c57d722d
Justin Hager <coachjustinhager@gmail.com>
2021-01-26 11:25:15 -0800

    add language fetch and state update

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,10 +1,48 @@
-import React, { Component } from 'react'
+import config from '../../config';
+import TokenService from '../../services/token-service';
+
+import React, { Component } from 'react';
 
 class DashboardRoute extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      language: '',
+      words: [],
+    }
+  }
+
+
+  componentDidMount(){
+    this.getLangAndWords();
+  }
+
+  getLangAndWords(){
+    fetch(`${config.API_ENDPOINT}/language`, {
+      method: 'GET',
+      headers: {
+        'authorization': `Bearer ${TokenService.getAuthToken()}`,
+      },
+    })
+    .then(response => response.json())
+    .then( result => {
+      this.setState({
+        language: result.language,
+        words: result.words
+      })
+    })  
+  }
+
   render() {
     return (
       <section>
-        implement and style me
+        api request
+        Subtitle with language name
+        button for 'start practicing'
+        Words to Practice: 
+         - list
+         - list
+         - list
       </section>
     );
   }
